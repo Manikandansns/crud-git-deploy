26dfb99729cc400ee68792a2fc5272462846f89d
Manikandan B <118624712+Manikandansns@users.noreply.github.com>
2024-05-23 01:36:40 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors(
     {
     origin: ["https://crud-git-deploy-frontend.vercel.app"],
-    methods: ["POST", "GET"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true
     }
     ));
