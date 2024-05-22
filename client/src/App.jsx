0bc39131c6ac6931a737a1f583a9dcd8c0b1c5f3
Manikandan B <118624712+Manikandansns@users.noreply.github.com>
2024-05-23 01:47:48 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
   const [editItem, setEditItem] = useState(null);
-  axios.defaults.withCredentials = true; 
 
   // Fetch items
   useEffect(() => {
